07d03a826a1f12f2df5adc061d171755a86468ee
Ковган Сергей Владимирович <Kovgan_SV@Gate.net>
2020-10-01 12:29:53 +0700

    feature: написал скрипт аккордеона секции консультации

--- js/main.js
+++ js/main.js
@@ -1,6 +1,50 @@
-const circle = document.querySelector('.circle'),
-  arrow = document.querySelector('.arrow');
+const accordeon = document.querySelector('.consultation-accordeon'),
+  accordeonPanels = accordeon.querySelectorAll('.consultation-panel');
 
-circle.addEventListener('click', () => {
-  arrow.classList.toggle('active');
+// скрипт аккордеона в секции Консультации
+function togglePanel(button) {
+  const panel = button.closest('.consultation-panel'),
+    arrow = button.querySelector('.arrow');
+
+    function closePanels() {
+      accordeonPanels.forEach(item => {
+        item.classList.remove('active');
+        item.querySelector('.arrow').classList.remove('active');
+      });
+    }
+
+    function openPanelAnimate(panel) {
+      let coin = 145;
+      const stop = panel.clientHeight;
+
+      requestAnimationFrame(function openPanelAnim() {
+        panel.style.height = coin + 'px';
+
+        if (coin < stop) {
+          coin += 15;
+          requestAnimationFrame(openPanelAnim);
+        } else {
+          panel.style.height = '';
+        }
+      });
+    }
+
+    if (panel.classList.contains('active')) {
+      panel.classList.remove('active');
+      arrow.classList.remove('active');
+    } else {
+      closePanels();
+      panel.classList.add('active');
+      arrow.classList.add('active');
+      openPanelAnimate(panel);
+    }
+}
+
+accordeon.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.closest('.consultation-panel__btn')) {
+    togglePanel(target.closest('.consultation-panel__btn'));
+  }
+
 });
